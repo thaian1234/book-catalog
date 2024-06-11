a4768873dcc430b6330e8fdcc32229da5761252a
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2024-06-11 09:56:11 +0700

    adding firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
-import { Button } from "@/components/ui/button";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
-export default function HomePage() {
-  return <Button className="flex items-center">HomePage</Button>;
+import { db } from "@/config/firebase";
+
+type Post = {
+  id: string;
+  title: string;
+};
+
+const getPosts = async () => {
+  const collectionRef = collection(db, "posts");
+  const postCollectionSnapshot = await getDocs(collectionRef);
+
+  const postList: Post[] = postCollectionSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    title: doc.data()?.title,
+  }));
+
+  return postList;
+};
+
+const getPostById = async (id: string) => {
+  const collectionRef = doc(db, "posts", id);
+  const docRef = await getDoc(collectionRef);
+
+  if (!docRef.exists()) {
+    return null;
+  }
+
+  return docRef.data() as Post;
+};
+
+export default async function HomePage() {
+  const posts = await getPosts();
+  const postId = await getPostById(posts[0].id);
+
+  console.log(postId);
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 }
