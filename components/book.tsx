79010053753315d0446006124f743d3f72c93d11
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2024-06-16 22:31:27 +0700

    fixing the UI in Book component

--- components/book.tsx
+++ components/book.tsx
@@ -13,10 +13,14 @@ export function Book({ book }: BookProps) {
   return (
     <div className="grid gap-2">
       <div className="flex gap-4">
-        <BookA width={64} height={120} />
         <div>
-          <h4 className="font-medium">{book.name}</h4>
-          <p className="text-gray-500">{book.authors.join(", ")}</p>
+          <BookA width={64} height={120} />
+        </div>
+        <div>
+          <h4 className="line-clamp-2 font-medium">{book.name}</h4>
+          <p className="line-clamp-1 text-gray-500">
+            {book.authors.join(", ")}
+          </p>
           <p className="text-sm text-gray-500">ISBN: {book.isbn}</p>
           <StarRating rating={book.rating || 0} />
         </div>
